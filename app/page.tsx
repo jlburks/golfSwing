25b6fdb7e518ce7b992f3597e539aac10f64accf
jlburks <jaren.burks1@gmail.com>
2026-05-27 11:46:07 -0500

    video emebed on app

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  const video = "/user1Swing.mp4";
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -33,6 +34,10 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
+          <video controls width="250">
+            <source src={video} type="video/mp4"/>
+          </video>
+  <source src="/shared-assets/videos/flower.webm" type="video/webm" />
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
