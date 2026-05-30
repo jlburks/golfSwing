c4a3968fadbff884934e9a70d3241da4e5600ea1
jlburks <jaren.burks1@gmail.com>
2026-05-30 09:19:31 -0500

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -17,26 +17,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          
           <video controls width="250">
             <source src={video1} type="video/mp4"/>
           </video>
@@ -46,6 +27,7 @@ export default function Home() {
           <video controls width="250">
             <source src={video3} type="video/mp4"/>
           </video>
+          
   <source src="/shared-assets/videos/flower.webm" type="video/webm" />
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
